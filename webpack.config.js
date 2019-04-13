3a460c13083afaee98431eb653877579918e5774
Garron Ma <garronma@gmail.com>
2019-04-13 13:51:05 -0700

    [4-13-19] Add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, '/src/index.js'),
+  entry: path.join(__dirname, '/src/client/static/index.js'),
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js'
@@ -15,5 +15,14 @@ module.exports = {
         use: 'babel-loader'
       }
     ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, '/src/client/static/index.html')
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
